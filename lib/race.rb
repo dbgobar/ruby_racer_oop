15079169ff39ec4b646c3b2de1d486cf4e4c4d21
Diana B. Gobar <dianabgobar@gmail.com>
2016-10-27 12:15:22 -0400

    rspec lab

--- lib/race.rb
+++ lib/race.rb
@@ -1,3 +1,27 @@
 class Race
+attr_accessor :name, :race_type , :distance, :km_times #used instead of the method def name, @name, end...
+
+ def initialize name, race_type, distance, km_times
+   @name = name
+   @race_type = race_type
+   @distance = distance
+   @km_times = km_times
+ end
+
+def total_time
+  @km_times.reduce(:+)
+end
+
+def slowest_km
+  @km_times.max
+end
+
+def quickest_km
+  @km_times.min
+end
+
+def avg_time_per_km
+  @km_times.reduce(:+)/@km_times.size.to_f #.size is like .length #to_f adds the decimals
+end
 
 end
